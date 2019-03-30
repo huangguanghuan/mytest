9c6512e116dfbabbf7680628c99182fe22c2a5b9
hh <huanhuan@itcast.cn>
2019-03-30 12:36:00 +0800

    day02

--- pinyougou/pinyougou-service/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
+++ pinyougou/pinyougou-service/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -8,30 +8,56 @@ import com.pinyougou.mapper.BrandMapper;
 import com.pinyougou.pojo.Brand;
 import com.pinyougou.service.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
  * 服务接口实现类
  */
 @Service(interfaceName = "com.pinyougou.service.BrandService")
+/* 上面指定接口名，产生服务名，不然会用代理类的名称 */
+@Transactional
 public class BrandServiceImpl implements BrandService {
 
-
+    /** 注入数据访问接口代理对象 */
     @Autowired
     private BrandMapper brandMapper;
+
+    @Override
+    public void save(Brand brand) {
+         brandMapper.insertSelective(brand);
+    }
+
+    @Override
+    public void update(Brand brand) {
+     brandMapper.updateByPrimaryKeySelective(brand);
+    }
+
+    @Override
+    public void delete(Serializable id) {
+
+    }
+
+    @Override
+    public void deleteAll(Serializable[] ids) {
+
+    }
+
+    @Override
+    public Brand findOne(Serializable id) {
+        return null;
+    }
+
     @Override
     public List<Brand> findAll() {
-        //开始分页
-        PageInfo<Brand> pageInfo = PageHelper.startPage(1,10)
-                .doSelectPageInfo(new ISelect() {
-            @Override
-            public void doSelect() {
-                brandMapper.selectAll();
-            }
-        });
-        System.out.println("总记录数"+pageInfo.getTotal());
-        System.out.println("总页数"+pageInfo.getPages());
-        return pageInfo.getList();
+
+       return brandMapper.selectAll();
+    }
+
+    @Override
+    public List<Brand> findByPage(Brand brand, int page, int rows) {
+        return null;
     }
 }
--- pinyougou/pinyougou-web/pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pinyougou/pinyougou-web/pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -3,8 +3,7 @@ package com.pinyougou.manager.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.Brand;
 import com.pinyougou.service.BrandService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,6 +11,7 @@ import java.util.List;
  * 品牌控制器
  */
 @RestController
+@RequestMapping("/brand")
 public class BrandController {
     /**
      * 引用服务接口代理对象
@@ -21,9 +21,33 @@ public class BrandController {
     @Reference(timeout = 20000)
     private BrandService brandService;
     /** 查询全部品牌 */
-    @GetMapping("/brand/findAll")
+    @GetMapping("/findAll")
     public List<Brand> findAll(){
 
         return brandService.findAll();
+    }
+    /** 添加品牌 */
+    @PostMapping("/save")
+    public boolean save(@RequestBody Brand brand){
+
+        try {
+            brandService.save(brand);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    /** 修改品牌 */
+    @PostMapping ("/update")
+    public boolean update(@RequestBody Brand brand ){
+
+        try {
+          brandService.update(brand);
+          return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
